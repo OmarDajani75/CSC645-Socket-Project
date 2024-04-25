9e99af626196edcb0d5bde4e48c223b36421fd99
Omar Dajani <omardajani079@gmail.com>
2024-04-25 11:34:30 -0700

    Fixed layout..? So werid, no idea what happened

--- TextClient.java
+++ TextClient.java
@@ -14,46 +14,45 @@ class TextClient {
               System.out.println("0. Connect to the server");
               System.out.println("1. Get the user list");
               System.out.println("2. Send a message");
-	      System.out.println("3. Get my messages");
-	      System.out.println("4. Exit");
+	          System.out.println("3. Get my messages");
+	          System.out.println("4. Exit");
               System.out.println("Enter a number:");
 
               String option = inFromUser.readLine();
               outToServer.writeBytes(option + "\n");
-
               switch (option) { 
                 case "0": 
-		    System.out.println("Please Enter Your Username: " + '\n');
-		    System.out.println("Please Enter Your Password: " + '\n');
-		    //Add if statement here
-		    System.out.println("Access Granted" + '\n');
-		    System.out.println("Access Denied - Incorrect Username/Password" + '\n');
-                    sentence = inFromUser.readLine(); 
-                    outToServer.writeBytes(sentence + '\n'); 
-                    modifiedSentence = inFromServer.readLine(); 
-                    System.out.println("FROM SERVER: " + modifiedSentence);
-                    break;
+		        System.out.println("Please Enter Your Username: " + '\n');
+		        System.out.println("Please Enter Your Password: " + '\n');
+		        //Add if statement here
+		        System.out.println("Access Granted" + '\n');
+		        System.out.println("Access Denied - Incorrect Username/Password" + '\n');
+                sentence = inFromUser.readLine(); 
+                outToServer.writeBytes(sentence + '\n'); 
+                modifiedSentence = inFromServer.readLine(); 
+                System.out.println("FROM SERVER: " + modifiedSentence);
+                break;
                 case "1": 
-                    sentence = inFromUser.readLine(); 
-                    outToServer.writeBytes(sentence + '\n'); 
-                    modifiedSentence = inFromServer.readLine(); 
-                    System.out.println("FROM SERVER: " + modifiedSentence);
-                    break;
+                sentence = inFromUser.readLine(); 
+                outToServer.writeBytes(sentence + '\n'); 
+                modifiedSentence = inFromServer.readLine(); 
+                System.out.println("FROM SERVER: " + modifiedSentence);
+                break;
                 case "2":
-		    sentence = inFromUser.readLine(); 
-		    outToServer.writeBytes(sentence + '\n'); 
-		    modifiedSentence = inFromServer.readLine(); 
-		    System.out.println("FROM SERVER: " + modifiedSentence);
-		    break;
-		case "3":
-		    sentence = inFromUser.readLine(); 
-		    outToServer.writeBytes(sentence + '\n'); 
-		    modifiedSentence = inFromServer.readLine(); 
-		    System.out.println("FROM SERVER: " + modifiedSentence);
-		    break;
-		case "4":    
-                    clientSocket.close();
-                    break;
+		        sentence = inFromUser.readLine(); 
+		        outToServer.writeBytes(sentence + '\n'); 
+		        modifiedSentence = inFromServer.readLine(); 
+		        System.out.println("FROM SERVER: " + modifiedSentence);
+		        break;
+		        case "3":
+		        sentence = inFromUser.readLine(); 
+		        outToServer.writeBytes(sentence + '\n'); 
+		        modifiedSentence = inFromServer.readLine(); 
+		        System.out.println("FROM SERVER: " + modifiedSentence);
+		        break;
+		        case "4":    
+                clientSocket.close();
+                break;
             }
             if (option.equals("4")) {
                 break;
